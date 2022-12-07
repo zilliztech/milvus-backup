7c8d78953a44ad1028118e27c35f9ea09eb0523b
“yelusion” <xun.huang@zilliz.com>
2022-12-07 21:46:32 +0800

    fix swagger bug.
    Signed-off-by: “yelusion” <xun.huang@zilliz.com>

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 // @contact.email  wayasxxx@gmail.com
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-// @host      localhost:8080
 // @BasePath  /api/v1
 func main() {
 	cmd.Execute()
