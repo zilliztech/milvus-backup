5488d00d90109f8b2b23017c34913599b8cbfe60
zhuwenxing <wenxing.zhu@zilliz.com>
2023-07-19 18:50:13 +0800

    Update version by goreleaser
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	_ "github.com/zilliztech/milvus-backup/docs"
 )
 
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
 // @title           Milvus Backup Service
 // @version         1.0
 // @description     A data backup & restore tool for Milvus
@@ -14,5 +20,6 @@ import (
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 // @BasePath  /api/v1
 func main() {
+	cmd.SetVersionInfo(version, commit, date)
 	cmd.Execute()
 }
