2a836fc8a4ec78dacc0ca130d9cf6d0e8dc553b9
zhuwenxing <wenxing.zhu@zilliz.com>
2023-09-04 19:20:20 +0800

    Update version by goreleaser (#191)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -22,4 +24,8 @@ func Execute() {
 	rootCmd.PersistentFlags().StringVarP(&config, "config", "", "backup.yaml", "config YAML file of milvus")
 
 	rootCmd.Execute()
+}
+
+func SetVersionInfo(version, commit, date string) {
+	rootCmd.Version = fmt.Sprintf("%s (Built on %s from Git SHA %s)", version, date, commit)
 }
