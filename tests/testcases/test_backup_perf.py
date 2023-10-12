ac245ded27f779d820bf16de1ec1fbc029d3e477
zhuwenxing <wenxing.zhu@zilliz.com>
2023-10-12 03:39:00 -0500

    [ci]Refine ci (#221)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/testcases/test_backup_perf.py
+++ tests/testcases/test_backup_perf.py
@@ -3,7 +3,6 @@ import pytest
 
 from base.client_base import TestcaseBase
 from common import common_func as cf
-from common import common_type as ct
 from common.common_type import CaseLabel
 from utils.util_log import test_log as log
 from api.milvus_backup import MilvusBackupClient
@@ -14,18 +13,21 @@ backup_prefix = "backup"
 client = MilvusBackupClient("http://localhost:8080/api/v1")
 
 
+@pytest.mark.tags(CaseLabel.Perf)
 class TestPerf(TestcaseBase):
     """ Test case of performance"""
 
-    @pytest.mark.tags(CaseLabel.L3)
     def test_milvus_create_backup_perf(self):
         # prepare data
-        total_nb = 1000000
+        total_nb = 10000
         cnt = 10
-        collection_to_backup = cf.gen_unique_str(c_name_prefix)
-        for i in range(cnt):
-            self.prepare_data(collection_to_backup, nb=total_nb // cnt)
-        collections_to_backup = [collection_to_backup]
+        coll_num = 2
+        collections_to_backup = []
+        for i in range(coll_num):
+            collection_to_backup = cf.gen_unique_str(c_name_prefix)
+            for j in range(cnt):
+                self.prepare_data(collection_to_backup, nb=total_nb // cnt)
+            collections_to_backup.append(collection_to_backup)
         checkers = {
             Op.create: BackupCreateChecker(collections_to_backup)
         }
@@ -39,15 +41,17 @@ class TestPerf(TestcaseBase):
         sleep(10)
         log.info("*********************Perf Test End**********************")
 
-    @pytest.mark.tags(CaseLabel.L3)
     def test_milvus_restore_backup_perf(self):
         # prepare data
-        total_nb = 1000000
+        total_nb = 10000
         cnt = 10
-        collection_to_backup = cf.gen_unique_str(c_name_prefix)
-        for i in range(cnt):
-            self.prepare_data(collection_to_backup, nb=total_nb // cnt)
-        collections_to_backup = [collection_to_backup]
+        coll_num = 2
+        collections_to_backup = []
+        for i in range(coll_num):
+            collection_to_backup = cf.gen_unique_str(c_name_prefix)
+            for j in range(cnt):
+                self.prepare_data(collection_to_backup, nb=total_nb // cnt)
+            collections_to_backup.append(collection_to_backup)
         backup_name = cf.gen_unique_str(backup_prefix)
         suffix = "_bak"
 
