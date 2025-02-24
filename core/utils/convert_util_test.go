409eba8bce91717adfb9aaaaf99f417b4249edc5
huanghaoyuanhhy <haoyuan.huang@zilliz.com>
2025-02-24 13:51:48 +0800

    backup: remove unnecessary conversion from vch to pch
    Signed-off-by: huanghaoyuanhhy <haoyuan.huang@zilliz.com>

--- core/utils/convert_util_test.go
+++ core/utils/convert_util_test.go
@@ -1,13 +1,31 @@
 package utils
 
 import (
+	"encoding/base64"
 	"testing"
+
+	"github.com/milvus-io/milvus-proto/go-api/v2/msgpb"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
 )
 
-func TestTs(t *testing.T) {
-	ts := 443727974068387848
-	time, logical := ParseTS(uint64(ts))
-	println(time.Unix())
-	println(logical)
+func TestBase64MsgPosition(t *testing.T) {
+	position := &msgpb.MsgPosition{
+		ChannelName: "channel",
+		MsgID:       []byte("msg_id"),
+		Timestamp:   10,
+		MsgGroup:    "msg_group",
+	}
 
+	str := Base64MsgPosition(position)
+	decodeByte, err := base64.StdEncoding.DecodeString(str)
+	assert.NoError(t, err)
+	decodePos := &msgpb.MsgPosition{}
+	err = proto.Unmarshal(decodeByte, decodePos)
+	assert.NoError(t, err)
+
+	assert.Equal(t, position.ChannelName, decodePos.ChannelName)
+	assert.Equal(t, position.MsgID, decodePos.MsgID)
+	assert.Equal(t, position.Timestamp, decodePos.Timestamp)
+	assert.Equal(t, position.MsgGroup, decodePos.MsgGroup)
 }
