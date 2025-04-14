98094fc25081d89fee6719c9dcf73ad3f71a6c32
zhuwenxing <wenxing.zhu@zilliz.com>
2025-04-14 20:00:23 +0800

    ci: fix get latest tag (#619)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- scripts/get_image_tag_by_short_name.py
+++ scripts/get_image_tag_by_short_name.py
@@ -4,13 +4,7 @@ from tenacity import retry, stop_after_attempt
 
 @retry(stop=stop_after_attempt(7))
 def get_image_tag_by_short_name(repository, tag, arch):
-
-    # Send API request to get all tags start with prefix
-    # ${branch}-latest  means the tag is a dev build
-        # master-latest -> master-$date-$commit
-        # 2.3.0-latest -> 2.3.0-$date-$commit
-    # latest means the tag is a release build
-        # latest -> v$version
+    
     splits = tag.split("-")
     prefix = f"{splits[0]}-" if len(splits) > 1 else "v"
     url = f"https://hub.docker.com/v2/repositories/{repository}/tags?name={prefix}&ordering=last_updated"
@@ -20,21 +14,19 @@ def get_image_tag_by_short_name(repository, tag, arch):
     # Get the latest tag with the same arch and prefix
     sorted_images = sorted(data["results"], key=lambda x: x["last_updated"], reverse=True)
     # print(sorted_images)
-    candidate_tag = None
+    # get the latest tag with the same arch and prefix
     for tag_info in sorted_images:
         # print(tag_info)
         image_name = tag_info["name"]
-        if tag == "2.2.0-latest": # special case for 2.2.0-latest, for 2.2.0 branch, there is no arm amd and gpu as suffix
-            candidate_tag = image_name
-        else:
-            if arch in image_name and len(image_name.split("-"))==4:
-                candidate_tag = tag_info["name"]
-            else:
-                continue
-        if candidate_tag == tag:
-            continue
-        else:
-            return candidate_tag
+        if arch in image_name and len(image_name.split("-"))==4:
+            return tag_info["name"]
+    # if no tag with the same arch and prefix, ignore arch and return the latest tag
+    for tag_info in sorted_images:
+        image_name = tag_info["name"]
+        if len(image_name.split("-"))==3:
+            return tag_info["name"]
+    # if no tag with the same prefix, return the short name tag directly
+    return tag
 
 
 if __name__ == "__main__":
