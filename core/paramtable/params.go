c8024552e7fffbcd5e4f1311f22a25253dab283f
Gifi Siby <gifi.s@ibm.com>
2025-04-24 11:10:35 +0530

    gcpnative support for backup tool (GCS implemented using Google Cloud Storage libraries) (#625)
    * GCS native support  (#444)
    
    Signed-off-by: gifi-siby <gifi.s@ibm.com>
    Signed-off-by: gifi-siby <gifi.s@ibm.com>
    
    * Removed hierarchical deletion removed and changed to flat deletion
    Signed-off-by: gifi-siby <gifi.s@ibm.com>
    
    Signed-off-by: gifi-siby <gifi.s@ibm.com>
    
    * Removed hierarchical deletion removed and changed to flat deletion
    
    Signed-off-by: gifi-siby <gifi.s@ibm.com>
    
    * Copy method using gcp native copy API
    Signed-off-by: gifi-siby <gifi.s@ibm.com>
    
    ---------
    
    Signed-off-by: gifi-siby <gifi.s@ibm.com>

--- core/paramtable/params.go
+++ core/paramtable/params.go
@@ -219,6 +219,7 @@ const (
 	S3                        = "s3"
 	CloudProviderAWS          = "aws"
 	CloudProviderGCP          = "gcp"
+	CloudProviderGCPNative    = "gcpnative"
 	CloudProviderAli          = "ali"
 	CloudProviderAliyun       = "aliyun"
 	CloudProviderAzure        = "azure"
@@ -232,6 +233,7 @@ var supportedStorageType = map[string]bool{
 	S3:                        true,
 	CloudProviderAWS:          true,
 	CloudProviderGCP:          true,
+	CloudProviderGCPNative:    true,
 	CloudProviderAli:          true,
 	CloudProviderAliyun:       true,
 	CloudProviderAzure:        true,
@@ -244,27 +246,29 @@ type MinioConfig struct {
 
 	StorageType string
 	// Deprecated
-	CloudProvider   string
-	Address         string
-	Port            string
-	AccessKeyID     string
-	SecretAccessKey string
-	UseSSL          bool
-	BucketName      string
-	RootPath        string
-	UseIAM          bool
-	IAMEndpoint     string
+	CloudProvider     string
+	Address           string
+	Port              string
+	AccessKeyID       string
+	SecretAccessKey   string
+	GcpCredentialJSON string
+	UseSSL            bool
+	BucketName        string
+	RootPath          string
+	UseIAM            bool
+	IAMEndpoint       string
 
-	BackupStorageType     string
-	BackupAddress         string
-	BackupPort            string
-	BackupAccessKeyID     string
-	BackupSecretAccessKey string
-	BackupUseSSL          bool
-	BackupBucketName      string
-	BackupRootPath        string
-	BackupUseIAM          bool
-	BackupIAMEndpoint     string
+	BackupStorageType       string
+	BackupAddress           string
+	BackupPort              string
+	BackupAccessKeyID       string
+	BackupSecretAccessKey   string
+	BackupGcpCredentialJSON string
+	BackupUseSSL            bool
+	BackupBucketName        string
+	BackupRootPath          string
+	BackupUseIAM            bool
+	BackupIAMEndpoint       string
 
 	CrossStorage bool
 }
@@ -277,6 +281,7 @@ func (p *MinioConfig) init(base *BaseTable) {
 	p.initPort()
 	p.initAccessKeyID()
 	p.initSecretAccessKey()
+	p.initGcpCredentialJSON()
 	p.initUseSSL()
 	p.initBucketName()
 	p.initRootPath()
@@ -289,6 +294,7 @@ func (p *MinioConfig) init(base *BaseTable) {
 	p.initBackupPort()
 	p.initBackupAccessKeyID()
 	p.initBackupSecretAccessKey()
+	p.initBackupGcpCredentialJSON()
 	p.initBackupUseSSL()
 	p.initBackupBucketName()
 	p.initBackupRootPath()
@@ -316,6 +322,11 @@ func (p *MinioConfig) initAccessKeyID() {
 func (p *MinioConfig) initSecretAccessKey() {
 	key := p.Base.LoadWithDefault("minio.secretAccessKey", DefaultMinioSecretAccessKey)
 	p.SecretAccessKey = key
+}
+
+func (p *MinioConfig) initGcpCredentialJSON() {
+	gcpCredentialJSON := p.Base.LoadWithDefault("minio.gcpCredentialJSON", DefaultGcpCredentialJSON)
+	p.GcpCredentialJSON = gcpCredentialJSON
 }
 
 func (p *MinioConfig) initUseSSL() {
@@ -426,6 +437,11 @@ func (p *MinioConfig) initBackupSecretAccessKey() {
 	key := p.Base.LoadWithDefault("minio.backupSecretAccessKey",
 		p.Base.LoadWithDefault("minio.secretAccessKey", DefaultMinioSecretAccessKey))
 	p.BackupSecretAccessKey = key
+}
+
+func (p *MinioConfig) initBackupGcpCredentialJSON() {
+	gcpCredentialJSON := p.Base.LoadWithDefault("minio.backupGcpCredentialJSON", DefaultGcpCredentialJSON)
+	p.BackupGcpCredentialJSON = gcpCredentialJSON
 }
 
 func (p *MinioConfig) initBackupBucketName() {
