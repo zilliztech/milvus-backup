f46a76f6cfe655e5c9733d4689c5e165759cdd6a
huanghaoyuanhhy <haoyuan.huang@zilliz.com>
2025-09-09 15:45:52 +0800

    cmd/restore: add --filter option (#779)
    Signed-off-by: huanghaoyuanhhy <haoyuan.huang@zilliz.com>

--- cmd/restore/restore_test.go
+++ cmd/restore/restore_test.go
@@ -4,9 +4,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/zilliztech/milvus-backup/core/restore"
 )
 
-func TestValidate(t *testing.T) {
+func TestOptions_validate(t *testing.T) {
 	t.Run("Valid", func(t *testing.T) {
 		var o options
 		o.backupName = "backup"
@@ -36,5 +37,28 @@ func TestValidate(t *testing.T) {
 		o.renameCollectionNames = "rename"
 		err := o.validate()
 		assert.Error(t, err)
+	})
+}
+
+func TestOptions_toTaskFilter(t *testing.T) {
+	t.Run("NoFilter", func(t *testing.T) {
+		var o options
+		dbFilter, collFilter, err := o.toTaskFilter()
+		assert.NoError(t, err)
+		assert.Empty(t, dbFilter)
+		assert.Empty(t, collFilter)
+	})
+
+	t.Run("Normal", func(t *testing.T) {
+		var o options
+		o.filter = "db1.*,db2.coll1,coll3,db3."
+		dbFilter, collFilter, err := o.toTaskFilter()
+		assert.NoError(t, err)
+		assert.Equal(t, map[string]struct{}{"db1": {}, "db2": {}, "db3": {}, "default": {}}, dbFilter)
+		assert.Equal(t, map[string]restore.CollFilter{
+			"db1":     {AllowAll: true},
+			"db2":     {CollName: map[string]struct{}{"coll1": {}}},
+			"default": {CollName: map[string]struct{}{"coll3": {}}},
+		}, collFilter)
 	})
 }
