f01981b59262f33ac5ca5a55e61f07fc449433dc
zhuwenxing <wenxing.zhu@zilliz.com>
2025-10-23 15:29:26 +0800

    test: add geometry datatype in test
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/testcases/test_restore_backup.py
+++ tests/testcases/test_restore_backup.py
@@ -1607,6 +1607,108 @@ class TestRestoreBackup(TestcaseBase):
         for name in restore_collections:
             self.compare_collections(name, name + suffix, verify_by_query=True)
 
+    @pytest.mark.parametrize("include_partition_key", [True])
+    @pytest.mark.parametrize("include_dynamic", [True])
+    @pytest.mark.tags(CaseLabel.L2)
+    def test_milvus_restore_back_with_geometry_datatype(self, include_dynamic, include_partition_key):
+        """Test backup and restore with GEOMETRY data type"""
+        self._connect()
+        name_origin = cf.gen_unique_str(prefix)
+        back_up_name = cf.gen_unique_str(backup_prefix)
+        fields = [
+            cf.gen_int64_field(name="int64", is_primary=True),
+            cf.gen_int64_field(name="key"),
+            cf.gen_json_field(name="json"),
+            cf.gen_geometry_field(name="geometry"),
+            cf.gen_float_vec_field(name="float_vector", dim=128),
+        ]
+        if include_partition_key:
+            partition_key = "key"
+            default_schema = cf.gen_collection_schema(
+                fields,
+                enable_dynamic_field=include_dynamic,
+                partition_key_field=partition_key,
+            )
+        else:
+            default_schema = cf.gen_collection_schema(
+                fields, enable_dynamic_field=include_dynamic
+            )
+
+        collection_w = self.init_collection_wrap(
+            name=name_origin, schema=default_schema, active_trace=True
+        )
+        nb = 3000
+        # Generate WKT geometry data
+        geometry_data = cf.gen_wkt_geometry(nb, bounds=(0, 100, 0, 100))
+
+        data = [
+            [i for i in range(nb)],
+            [i % 3 for i in range(nb)],
+            [{f"key_{str(i)}": i} for i in range(nb)],
+            geometry_data,
+            [[np.float32(i) for i in range(128)] for _ in range(nb)],
+        ]
+        collection_w.insert(data=data)
+
+        if include_dynamic:
+            # Insert additional data with dynamic fields
+            data_with_dynamic = [
+                {
+                    "int64": i,
+                    "key": i % 3,
+                    "json": {f"key_{str(i)}": i},
+                    "geometry": geometry_data[i % len(geometry_data)],
+                    "float_vector": [np.float32(i) for i in range(128)],
+                    f"dynamic_{str(i)}": i,
+                }
+                for i in range(nb, nb * 2)
+            ]
+            collection_w.insert(data=data_with_dynamic)
+
+        res = self.client.create_backup(
+            {
+                "async": False,
+                "backup_name": back_up_name,
+                "collection_names": [name_origin],
+            }
+        )
+        log.info(f"create_backup {res}")
+        res = self.client.list_backup()
+        log.info(f"list_backup {res}")
+        if "data" in res:
+            all_backup = [r["name"] for r in res["data"]]
+        else:
+            all_backup = []
+        assert back_up_name in all_backup
+        backup = self.client.get_backup(back_up_name)
+        assert backup["data"]["name"] == back_up_name
+        backup_collections = [
+            backup["collection_name"] for backup in backup["data"]["collection_backups"]
+        ]
+        assert name_origin in backup_collections
+        res = self.client.restore_backup(
+            {
+                "async": False,
+                "backup_name": back_up_name,
+                "collection_names": [name_origin],
+                "collection_suffix": suffix,
+            }
+        )
+        log.info(f"restore_backup: {res}")
+        res, _ = self.utility_wrap.list_collections()
+        assert name_origin + suffix in res
+        output_fields = None
+        self.compare_collections(
+            name_origin, name_origin + suffix, output_fields=output_fields
+        )
+        res = self.client.delete_backup(back_up_name)
+        res = self.client.list_backup()
+        if "data" in res:
+            all_backup = [r["name"] for r in res["data"]]
+        else:
+            all_backup = []
+        assert back_up_name not in all_backup
+
     @pytest.mark.tags(CaseLabel.L0)
     def test_milvus_restore_backup_with_ttl_property(self):
         """Verify that collection TTL property can be backed up and restored correctly"""
