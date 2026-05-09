327f3800aa70bfea9f487a8d32de51f7ce3aa1d2
Xuewei Wang <xueweiwang.sallery@gmail.com>
2026-05-09 17:41:32 +0800

    Fix empty restore import paths
    Signed-off-by: Xuewei Wang <xueweiwang.sallery@gmail.com>

--- core/restore/secondary/coll_dml_task_test.go
+++ core/restore/secondary/coll_dml_task_test.go
@@ -141,6 +141,41 @@ func newTestCollBackup(vchannels []string, partitions []*backuppb.PartitionBacku
 	}
 }
 
+func TestPartitionDirToPaths(t *testing.T) {
+	tests := []struct {
+		name string
+		dir  partitionDir
+		want []string
+	}{
+		{
+			name: "insert and delta",
+			dir:  partitionDir{insertLogDir: "insert", deltaLogDir: "delta"},
+			want: []string{"insert", "delta"},
+		},
+		{
+			name: "insert only",
+			dir:  partitionDir{insertLogDir: "insert"},
+			want: []string{"insert"},
+		},
+		{
+			name: "delta only",
+			dir:  partitionDir{deltaLogDir: "delta"},
+			want: []string{"delta"},
+		},
+		{
+			name: "empty",
+			dir:  partitionDir{},
+			want: []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, tt.dir.toPaths())
+		})
+	}
+}
+
 func newTestDMLTask(t *testing.T, collBackup *backuppb.CollectionBackupInfo, stream *recordingStream) *collDMLTask {
 	vchannels := collBackup.GetVirtualChannelNames()
 
