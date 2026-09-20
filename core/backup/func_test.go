8689668531bea35bba53cbf3726a37f4034fd9a9
huanghaoyuanhhy <haoyuan.huang@zilliz.com>
2026-09-20 12:04:13 +0800

    test: drop tautological strategy/format parse tests
    Signed-off-by: huanghaoyuanhhy <haoyuan.huang@zilliz.com>

--- core/backup/func_test.go
+++ core/backup/func_test.go
@@ -14,24 +14,11 @@ func TestDefaultName(t *testing.T) {
 	assert.Equal(t, "backup_1970_01_01_00_01_40_100", name)
 }
 
-func TestSupportStrategy(t *testing.T) {
-	strategies := SupportStrategy()
-	assert.Len(t, strategies, len(_strategyMap))
-}
-
 func TestParseStrategy(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		s, err := ParseStrategy("")
 		assert.NoError(t, err)
 		assert.Equal(t, StrategyAuto, s)
-	})
-
-	t.Run("Valid", func(t *testing.T) {
-		for _, strategy := range SupportStrategy() {
-			s, err := ParseStrategy(strategy)
-			assert.NoError(t, err)
-			assert.Equal(t, _strategyMap[strategy], s)
-		}
 	})
 
 	t.Run("SnapshotIsNotAStrategy", func(t *testing.T) {
@@ -46,24 +33,11 @@ func TestParseStrategy(t *testing.T) {
 	})
 }
 
-func TestSupportFormat(t *testing.T) {
-	formats := SupportFormat()
-	assert.Len(t, formats, len(_formatMap))
-}
-
 func TestParseFormat(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		f, err := ParseFormat("")
 		assert.NoError(t, err)
 		assert.Equal(t, FormatAuto, f)
-	})
-
-	t.Run("Valid", func(t *testing.T) {
-		for _, format := range SupportFormat() {
-			f, err := ParseFormat(format)
-			assert.NoError(t, err)
-			assert.Equal(t, _formatMap[format], f)
-		}
 	})
 
 	t.Run("Invalid", func(t *testing.T) {
